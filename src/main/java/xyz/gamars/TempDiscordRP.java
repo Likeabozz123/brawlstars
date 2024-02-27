4dce9e1c5512e3fc49b022272928e9475d8cda34
Daryan <trinhdaryan@gmail.com>
2024-02-27 11:20:11 -0800

    Kryonet & Discord Game SDK Libraries
    - Added debug version of Kryonet
    - Added discord-game-sdk4j library to handle Discord Game SDK
    - Created temporary discord rich presence handling

---
+++ src/main/java/xyz/gamars/TempDiscordRP.java
@@ -0,0 +1,54 @@
+package xyz.gamars;
+
+import de.jcm.discordgamesdk.Core;
+import de.jcm.discordgamesdk.CreateParams;
+import de.jcm.discordgamesdk.activity.Activity;
+
+import java.io.File;
+import java.time.Instant;
+
+public class TempDiscordRP {
+
+    public void start() {
+
+        // Initialize the Core
+        Core.init(new File("discord_game_sdk.dll"));
+        try(CreateParams params = new CreateParams())
+        {
+            params.setClientID(1212112030203519047L);
+            params.setFlags(CreateParams.getDefaultFlags());
+
+            try(Core core = new Core(params))
+            {
+                try(Activity activity = new Activity())
+                {
+                    activity.setDetails("Playing Brawlstars");
+
+                    // Setting a start time causes an "elapsed" field to appear
+                    activity.timestamps().setStart(Instant.now());
+
+                    activity.assets().setLargeImage("main_logo");
+
+                    // Finally, update the current activity to our activity
+                    core.activityManager().updateActivity(activity);
+                }
+
+                // Run callbacks forever
+                while(true)
+                {
+                    core.runCallbacks();
+                    try
+                    {
+                        // Sleep a bit to save CPU
+                        Thread.sleep(16);
+                    }
+                    catch(InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+}

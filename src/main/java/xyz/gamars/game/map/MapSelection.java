52bf573c03348595377a73e8b4b7391d51f2bcb8
saipisupati <sai.pisupati.030@gmail.com>
2024-05-15 14:35:08 -0700

    UPDATED JAVADOC COMMENTS
    forgot to add javadoc comment to MapSelection class

--- src/main/java/xyz/gamars/game/map/MapSelection.java
+++ src/main/java/xyz/gamars/game/map/MapSelection.java
@@ -1,5 +1,9 @@
 package xyz.gamars.game.map;
 
+/**
+ * Selects which map to play as background
+ * @author Daryan, Vishak, Sai
+ */
 public enum MapSelection {
 
     UNDERGROUND(0, "underground", 21, 33),
@@ -19,21 +23,47 @@ public enum MapSelection {
         this.maxWorldHeight = maxWorldHeight;
     }
 
+    /**
+     * gets the ID of the map
+     * @return the map ID
+     * @author Daryan, Vishak, Sai
+     */
     public int getMapID() {
         return mapID;
     }
 
+    /**
+     * gets the map's width
+     * @return the width of the map
+     * @author Daryan, Vishak, Sai
+     */
     public int getMaxWorldWidth() {
         return maxWorldWidth;
     }
 
+    /**
+     * get's the map's height
+     * @return the height of the map
+     * @author Daryan, Vishak, Sai
+     */
     public int getMaxWorldHeight() {
         return maxWorldHeight;
     }
 
+    /**
+     * Gets the folder with the correct map
+     * @return folder with the needed map
+     * @author Daryan, Vishak, Sai
+     */
     public String getFolder() {
         return folder;
     }
+
+    /**
+     * Gets the map's name
+     * @return a string with the map's name
+     * @author Daryan, Vishak, Sai
+     */
 
     public String getMapName() {
         return name().charAt(0) + name().substring(1).toLowerCase();
